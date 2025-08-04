2c925724657c00cf737a9b0fae845887e153e8a6
enzo-binatti <enzo.baleroni@gmail.com>
2025-08-04 07:35:39 -0300

    Delete exemplo de superglobais directory

--- exemplo de superglobais/Importação de arquivos/include/header.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <title><?php echo $tituloPagina; ?></title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <div class="container">
--- exemplo de superglobais/Importação de arquivos/require_once/funcoes.php
+++
@@ -1,9 +0,0 @@
-<?php
-function sanitizar($dados) {
-    return htmlspecialchars(trim($dados));
-}
-
-function formatarData($data) {
-    return date('d/m/Y', strtotime($data));
-}
-?>
