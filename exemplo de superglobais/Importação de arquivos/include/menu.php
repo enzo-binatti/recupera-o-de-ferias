0b7d5fe9c9d9c186898b8a4a4ec784776c70ed58
enzo-binatti <enzo.baleroni@gmail.com>
2025-08-04 07:36:27 -0300

    Add files via upload

---
+++ exemplo de superglobais/Importação de arquivos/include/menu.php
@@ -0,0 +1,7 @@
+<nav class="menu">
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="sobre.php">Sobre</a></li>
+        <li><a href="contato.php">Contato</a></li>
+    </ul>
+</nav>
